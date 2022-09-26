8745a25b3f5e0549c7cdf72b1b3911938e8aa178
vishal Thakur <vish75810@gmail.com>
2022-09-27 02:51:59 +0530

    Add Comment Notification

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -13,11 +13,31 @@ export default class extends Controller {
     event.stopPropagation();
     const formID = event.params["form"]; 
     const commentBodyID = event.params["body"];
-    const form = document.getElementById(formID)
+    const editButtonID = event.params["edit"];
+
+    const form = document.getElementById(formID);
+    const commentBody = document.getElementById(commentBodyId);
+    const editButton = document.getElementById(editButtonID);
+
+
     form.classList.toggle("d-none");
     form.classList.toggle("mt-5");
-    const commentBody = document.getElementById(commentBodyId);
     commentBody.classList.toggle("d-none");
+    this.toggleEditButton(editButton);
 
   }
+  toggleEditButton(editButton) {
+    if (editButton.innerText === "Edit") {
+        editButton.innerText = "Cancel";
+        this.toggleEditButtonClass(editButton);
+    }
+    else{
+        editButton.innerText = "Edit";
+        this.toggleEditButtonClass(editButton);
+    }
+}
+    toggleEditButtonClass(editButton) {
+        editButton.classList.toggle("btn-secondary");
+        editButton.classList.toggle("btn-warning");
+    }
 }
